01c20debfed41a2189396d96164b15340ebb33b4
ramstein74 <ramstein74@gmail.com>
2015-12-17 12:50:21 +0000

    quadro com detalhes

--- src/components/details.vue
+++ src/components/details.vue
@@ -14,10 +14,21 @@ td
 
 </style>
 
-<template lang="jade">
-
-
-|here i come
+div
+  textarea(name="observacoes", style="width: 600px; height: 100px;display:block;", placeholder="Observações", v-model="observ")
+  input(type="text", v-model="formula", style="width:300px;", placeholder="Formula")
+  input(type="text", v-model="cliente", style="width:300px;", placeholder="Cliente")
+  input(type="date", style="width:145px;", v-model="dataf")
+  input(type="text", v-model="aplicacao", placeholder="Aplicação")
+  input(type="text", v-model="dev", placeholder="Dev")
+  input(type="text", v-model="pedido", style="width:50px;", placeholder="Pedido")
+  br
+  span.searchClient(v-on="click:setCliente(cli)", v-repeat="cli:clientes | filterBy cliente | first 5")
+    | (%cli.nome%)
+  span.searchAplica(v-on="click:setAplicacao(aplica)", v-repeat="aplica: aplicacoes |filterBy aplicacao| first 5")
+    | (%aplica%)
+  span.searchFormula(v-on="click:setFormula(cor)", v-repeat="cor: cores |filterBy formula |first 5")
+    | (%cor%)
 
 </template>
 
